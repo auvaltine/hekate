96b19afa84fca5943d5ef3b93d7dbfca40658f63
Abhayā Auva <abhaya@hekate.app>
2022-12-25 16:48:09 -0500

    socket.js: removed unnecessary code

--- client/core/socket.js
+++ client/core/socket.js
@@ -3,18 +3,12 @@ Hekate.socket = (() => {
 		events = [];
 		instance = 0;
 		constructor (url, opts = {}) {
-			if (typeof url === 'string' && url) {
-				this.socket = new WebSocket(url[0] === '/'
-					? `ws${window.location.protocol === 'https:' ? 's' : ''}://${window.location.host}${url}`
-					: (url = new URL(url)) && `ws${url.protocol === 'https:' ? 's' : ''}://${url.host}${url.pathname}`);
-				this.socket.onclose = this.onclose.bind(this);
-				this.socket.onerror = this.onerror.bind(this);
-				this.socket.onmessage = this.onmessage.bind(this);
-				this.socket.onopen = this.onopen.bind(this);
-			}
+			url = typeof url === 'string' && url ? url : '/';
+			url = new URL((url[0] === '/' ? `http${location.protocol === 'https:' ? 's' : ''}://${location.host}` : '') + url);
+			this.socket = new WebSocket(`ws${url.protocol === 'https:' ? 's' : ''}://${url.host}${url.pathname}`);
+			this.socket.onmessage = this.onmessage.bind(this);
+			this.socket.onopen = this.onopen.bind(this);
 		};
-		onclose (event) {};
-		onerror (event) {};
 		onmessage (event, data) {
 			switch (data = event.data) {
 				case 'ping': return this.socket.send('pong'); // server heartbeat
@@ -28,9 +22,7 @@ Hekate.socket = (() => {
 				}
 			}
 		};
-		onopen () {
-			this.onmessage({ data: JSON.stringify({ event: 'open' }) });
-		};
+		onopen () { this.onmessage({ data: JSON.stringify({ event: 'open' }) }); };
 		on (event, fn) { this.events.push({ event, fn }); };
 		send (event, data) { this.socket.send(JSON.stringify({ event, data })); };
 	};
